ef771c1c9d13e1f964ab9b3c847094ca1863ab5f
dlrw-sys <dlrwsxpro@gmail.com>
2026-02-12 23:01:18 +0300

    Update example_js_snippet.js

--- example_js_snippet.js
+++ example_js_snippet.js
@@ -1,4 +1,40 @@
-window.pywebview.api.external_drag({
-    paths: ["D:\\example.png"],
-    ctrl: false
-});
+let isNativeDragging = false;
+
+li.onmousedown = e => {
+    if (e.button !== 0) return;
+
+    const startX = e.screenX;
+    const startY = e.screenY;
+
+    const handleMove = moveEvent => {
+
+        if (Math.abs(moveEvent.screenX - startX) > 15 ||
+            Math.abs(moveEvent.screenY - startY) > 15) {
+
+            if (isNativeDragging) return;
+            isNativeDragging = true;
+
+            document.removeEventListener('mousemove', handleMove);
+
+            const selection = selectionMap[side];
+
+            console.log('[NATIVE DRAG] Calling Python');
+
+            window.pywebview.api.external_drag({
+                paths: [...selection],
+                ctrl: e.ctrlKey
+            }).finally(() => {
+                console.log('[NATIVE DRAG] finished');
+                isNativeDragging = false;
+            });
+        }
+    };
+
+    const handleUp = () => {
+        document.removeEventListener('mousemove', handleMove);
+        document.removeEventListener('mouseup', handleUp);
+    };
+
+    document.addEventListener('mousemove', handleMove);
+    document.addEventListener('mouseup', handleUp);
+};
